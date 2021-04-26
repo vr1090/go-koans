6ec5dae78726cd42a865352f75445866a0f7b173
inu rumah <inurumah@vadhiMac-2.local>
2021-04-26 20:39:22 +0700

    update training

--- about_strings.go
+++ about_strings.go
@@ -3,12 +3,12 @@ package go_koans
 import "fmt"
 
 func aboutStrings() {
-	assert("a"+__string__ == "abc") // string concatenation need not be difficult
-	assert(len("abc") == __int__)   // and bounds are thoroughly checked
+	assert("a"+"bc" == "abc") // string concatenation need not be difficult
+	assert(len("abc") == 3)   // and bounds are thoroughly checked
 
-	assert("abc"[0] == __byte__) // their contents are reminiscent of C
+	assert("abc"[0] == 'a') // their contents are reminiscent of C
 
-	assert("smith"[2:] == __string__)  // slicing may omit the end point
+	assert("smith"[2:] == "ith")       // slicing may omit the end point
 	assert("smith"[:4] == __string__)  // or the beginning
 	assert("smith"[2:4] == __string__) // or neither
 	assert("smith"[:] == __string__)   // or both
--- setup_koans_test.go
+++ setup_koans_test.go
@@ -15,8 +15,8 @@ const (
 	__int__          int     = -1
 	__positive_int__ int     = 42
 	__byte__         byte    = 255
-	__bool__         bool    = false // ugh
-	__boolean__      bool    = true  // oh well
+	__bool__         bool    = true // ugh
+	__boolean__      bool    = true // oh well
 	__float32__      float32 = -1.0
 	__delete_me__    bool    = false
 )
