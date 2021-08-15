89a7bffff0f6b802ab3ed65e415adbfc105d81c5
inu <inu.pragantha@gmail.com>
2021-08-15 16:51:13 +0700

    concurrency

--- about_concurrency.go
+++ about_concurrency.go
@@ -1,5 +1,7 @@
 package go_koans
 
+import "fmt"
+
 func isPrimeNumber(possiblePrime int) bool {
 	for underPrime := 2; underPrime < possiblePrime; underPrime++ {
 		if possiblePrime%underPrime == 0 {
@@ -12,6 +14,11 @@ func isPrimeNumber(possiblePrime int) bool {
 func findPrimeNumbers(channel chan int) {
 	for i := 2; ; /* infinite loop */ i++ {
 		// your code goes here
+		fmt.Println("ini msuk sini", i)
+		if isPrimeNumber(i) {
+			channel <- i
+			fmt.Println("prime number", i)
+		}
 
 		assert(i < 100) // i is afraid of heights
 	}
@@ -20,9 +27,10 @@ func findPrimeNumbers(channel chan int) {
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
+	assert( len(ch) == 0) // concurrency can be almost trivial
 	// your code goes here
-
+	go findPrimeNumbers(ch)
+d
 	assert(<-ch == 2)
 	assert(<-ch == 3)
 	assert(<-ch == 5)
